d4832b68c7472cb48d712067f51c43bd03538e44
Evan Fisher-Perez <evanfp582@gmail.com>
2020-05-02 23:16:32 -0400

    Update reactionGame.py

--- reactionGame.py
+++ reactionGame.py
@@ -1,6 +1,37 @@
 import pyautogui
-import tkinter
+import tkinter as tk
 import sqlite3
 
 screenW, screenH = pyautogui.size()
 
+
+window = tk.Tk()
+
+entry = tk.Entry(fg="yellow", bg="blue", width=50)
+entry.insert(0, "Username")
+entry.pack()
+
+label = tk.Label(
+    text="Reaction Test",
+    foreground="white",  
+    background="black"  
+)
+label.pack()
+gameButton = tk.Button(
+    text="To Go To Game",
+    width=25,
+    height=5,
+    bg="white",
+    fg="black",
+)
+gameButton.pack()
+highScoreButton = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="white",
+    fg="black",
+)
+highScoreButton.pack()
+
+window.mainloop()
