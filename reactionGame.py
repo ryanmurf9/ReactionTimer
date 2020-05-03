158e06e168e4fb7ee44e9c35eb1b2c46be3418ef
Evan Fisher-Perez <evanfp582@gmail.com>
2020-05-03 01:23:03 -0400

    Update reactionGame.py

--- reactionGame.py
+++ reactionGame.py
@@ -1,36 +1,59 @@
 import pyautogui
 import tkinter as tk
 import sqlite3
+import re
 
 screenW, screenH = pyautogui.size()
 
 
+text=pyautogui.size()
+
+
+#Change resolution from pyautogui format to tKinter format
+def resolution():
+    text=str(pyautogui.size())
+    size= re.sub(r'''(Size)|width=|,|height=|([()])''',"",text)
+    size=size.split()
+    resolution=size[0] +"x"+ size[1]
+    return[resolution]
+
+
+
 window = tk.Tk()
+window.geometry(resolution())
+#Must be in window.geometry("500x500") format
+
 
 entry = tk.Entry(fg="yellow", bg="blue", width=50)
 entry.insert(0, "Username")
 entry.pack()
 
 label = tk.Label(
+    padx=100,
+    pady=100,
     text="Reaction Test",
-    foreground="white",  
-    background="black"  
+    foreground="black",  
+    background="#f0f0f0"
 )
 label.pack()
 gameButton = tk.Button(
+    padx=100,
+    pady=100,
     text="To Go To Game",
     width=25,
     height=5,
-    bg="white",
-    fg="black",
+    foreground="black",  
+    background="#f0f0f0"
 )
 gameButton.pack()
 highScoreButton = tk.Button(
-    text="Click me!",
+    padx=100,
+    pady=100,
+    text="High Scores",
     width=25,
     height=5,
-    bg="white",
-    fg="black",
+    foreground="black",  
+    background="#f0f0f0"
 )
 highScoreButton.pack()
 
