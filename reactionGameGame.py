05e1019895bb75c3db1d68a3f0db480ea5b363b5
Evan Fisher-Perez <evanfp582@gmail.com>
2020-05-03 15:49:54 -0400

    Made button apear and reapear in differnce positions

--- reactionGameGame.py
+++ reactionGameGame.py
@@ -1,3 +1,39 @@
 import pyautogui
 import tkinter as tk
 import sqlite3
+import time
+
+
+def spawnNewButton():
+    global x
+    x=x+50
+    global y
+    y=y+50
+    gameButton.place(x=x, y=y)
+
+    
+def clicked():
+    print("pooPooPeepee")
+    gameButton.place_forget()
+    spawnNewButton()
+
+#Sorts out resolution
+screenW, screenH = pyautogui.size()
+resolution=str(screenW)+"x"+str(screenH)
+window = tk.Tk()
+window.title("Reaction Game")
+window.geometry(resolution)
+#
+global gameButton
+gameButton= tk.Button(
+        window,
+        width=10,
+        height=5, 
+        background="red",
+        command=clicked 
+    )
+
+x=100
+y=100
+startTime=time.time()
+spawnNewButton()
